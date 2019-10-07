c17ca484dc5fd392bb700ca4ba0eee619d6c0e19
Kyle Yu <gystre@gmail.com>
2019-10-06 18:38:18 -0700

    added a stronghold calculator page

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const navSlide = () =>{
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
-    const resize_images = document.querySelector(".resizable-image");
+    //const resize_images = document.querySelector(".resizable-image");
 
-    resize_images.addEventListener("click", () => {
+    //resize_images.addEventListener("click", () => {
         // var new_image = document.createElement("img");
         // new_image.position = "absolute";
         // new_image.src = `${resize_images.src}`;
         // document.body.appendChild(new_image);
-    });
+    //});
 
     //toggle nav
     burger.addEventListener("click", () => {
@@ -32,12 +32,16 @@ const navSlide = () =>{
 navSlide();
 
 
-$(document).ready(function(){      
-    setInterval(function(){
-        document.getElementById("date").innerHTML = new Date();
-    } , 100);
+$(document).ready(function(){
+    if(document.getElementById("date")){
+        setInterval(function(){
+            document.getElementById("date").innerHTML = new Date();
+        } , 100);
+    }
     
+    const nav = document.querySelector(".nav-links");
     var prevScrollpos = window.pageYOffset;
+    
     window.onscroll = function() {
         var currentScrollPos = window.pageYOffset;
         
@@ -45,6 +49,10 @@ $(document).ready(function(){
             document.getElementById("nav-bar").style.top = "0";
         } else {
             document.getElementById("nav-bar").style.top = "-75px";
+
+            if(nav.classList.toggle("nav-active")){
+                nav.classList.toggle("nav-active");
+            }
         }
         prevScrollpos = currentScrollPos;
     }
