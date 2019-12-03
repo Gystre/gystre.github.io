65107d6ebe8f55fc452b27e5d60241c3e99a9602
Kyle Yu <gystre@gmail.com>
2019-12-02 22:08:11 -0800

    moved stuff around
    pushing now so I can work on timer at school

--- timer/timer.js
+++ timer/timer.js
@@ -1,35 +1,42 @@
-var sec = 0;
+var totalSeconds = 0;
 var started = false;
 var countdown;
 
-function timedCount(){
-    document.getElementById("time").innerHTML = sec;
-    sec--;
-    countdown = setTimeout(timedCount, 1000);
-}
+// function timedCount(){
+//     document.getElementById("time").innerHTML = sec;
+//     if(totalSeconds == 0){
+//         stop();
+//     }else{
+//         totalSeconds--;
+//         countdown = setTimeout(timedCount, 1000);
+//     }
+// }
 
 function start(){
-    if(!started){
-        started = true;
-        timedCount();
-    }
+    var h = parseInt(document.getElementById("hours").value.length == 0 ? 0 : document.getElementById("hours").value);
+    var m = parseInt(document.getElementById("minutes").value.length == 0 ? 0 : document.getElementById("minutes").value);
+    var s = parseInt(document.getElementById("seconds").value.length == 0 ? 0 : document.getElementById("seconds").value);
+
+    totalSeconds = (h*3600) + (m*60) + s;
+
+    var totalMinutes = totalSeconds / 60;
+    var totalHours = totalMinutes / 60;
+
+    // if(!started){
+    //     started = true;
+    //     timedCount();
+    // }
+
 }
 
-function stop(){
-    clearTimeout(countdown);
-    started = false;
-}
+// function stop(){
+//     clearTimeout(countdown);
+//     started = false;
+// }
 
-function reset(){
-    sec = document.getElementById("seconds").value;
-    document.getElementById("time").innerHTML = sec;
-    clearTimeout(countdown);
-    started = false;
-}
-
-$(document).ready(function(){
-    $("#seconds").change(function(){
-        sec = document.getElementById("seconds").value;
-        document.getElementById("time").innerHTML = sec;    
-    })
-})
+// function reset(){
+//     totalSeconds = document.getElementById("seconds").value;
+//     document.getElementById("time").innerHTML = totalSeconds;
+//     clearTimeout(countdown);
+//     started = false;
+// }
