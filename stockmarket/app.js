8c8f00c67823f1bef06cb24e3f45f47ce4e6dd7e
Kyle Yu <gystre@gmail.com>
2020-03-10 18:43:05 -0700

    fixed minor bug

--- stockmarket/app.js
+++ stockmarket/app.js
@@ -5,17 +5,17 @@ document.addEventListener('keydown', function(event) {
         var stock3worth = parseFloat(document.getElementById("stock3").value) * parseFloat(document.getElementById("stock3amt").value);
         var stock4worth = parseFloat(document.getElementById("stock4").value) * parseFloat(document.getElementById("stock4amt").value);
 
-        document.getElementById("stock1worth").innerHTML = stock1worth;
-        document.getElementById("stock2worth").innerHTML = stock2worth;
-        document.getElementById("stock3worth").innerHTML = stock3worth;
-        document.getElementById("stock4worth").innerHTML = stock4worth;
+        document.getElementById("stock1worth").innerHTML = "$" + stock1worth.toFixed(2);
+        document.getElementById("stock2worth").innerHTML = "$" + stock2worth.toFixed(2);
+        document.getElementById("stock3worth").innerHTML = "$" + stock3worth.toFixed(2);
+        document.getElementById("stock4worth").innerHTML = "$" + stock4worth.toFixed(2);
 
         var total = stock1worth + stock2worth  + stock3worth + stock4worth;
         document.getElementById("totalvalue").innerHTML = total;
-        document.getElementById("lossorgain").innerHTML = total - 100000;
+        document.getElementById("lossorgain").innerHTML = (total - 100000).toFixed(2);
 
         document.getElementById("dowchange").innerHTML = parseFloat(document.getElementById("dowstart").value) - parseFloat(document.getElementById("dowmarketday").value)
-        document.getElementById("naschange").innerHTML = parseFloat(docuemnt.getElementById("nasstart").value) - parseFloat(document.getElementById("nasmarketday").value)
+        document.getElementById("naschange").innerHTML = parseFloat(document.getElementById("nasstart").value) - parseFloat(document.getElementById("nasmarketday").value)
         
     }
 });
